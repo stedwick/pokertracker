1120924f729cb3dfab2a1956a8257f56ef9a050d
Philip Brocoum <philip.brocoum@gmail.com>
2022-09-14 22:19:48 -0700

    Successful Google sign-in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,104 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {signInWithRedirect, signOut, getRedirectResult, onAuthStateChanged, GoogleAuthProvider} from "firebase/auth";
+import {auth, authGoogle} from "./utils/firebase";
+
+var currentUser = false;
+
+// onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//         // User is signed in, see docs for a list of available properties
+//         // https://firebase.google.com/docs/reference/js/firebase.User
+//         const uid = user.uid;
+//         currentUser = user;
+//         // ...
+//     } else {
+//         // User is signed out
+//         // ...
+//     }
+// });
+
+function pokerSignIn() {
+    signInWithRedirect(auth, authGoogle);
+}
+
+function pokerSignOut() {
+    signOut(auth).then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
+    });
+}
+
+class CurrentUser extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {myUser: currentUser};
+    }
+    render() {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                const uid = user.uid;
+                this.setState({myUser: user});
+                // ...
+            } else {
+                this.setState({myUser: null});
+                // User is signed out
+                // ...
+            }
+        });
+        if (this.state.myUser) {
+            return (
+                <p>Welcome {this.state.myUser.email}!</p>
+            );
+        } else {
+            return (
+                <p>Nobody logged in.</p>
+            );
+        }
+    }
+}
 
 function App() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const apiKey = urlParams.get('apiKey');
+    if (apiKey) {
+        getRedirectResult(auth)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access Google APIs.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+
+                // The signed-in user info.
+                currentUser = result.user;
+            }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // alert(errorMessage);
+            // The email of the user's account used.
+            // const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+    }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={pokerSignIn}>
+          ✅ Activate Lasers
+        </button>
+          <button onClick={pokerSignOut}>
+              🚪 Sign Out
+          </button>
+          <CurrentUser />
         <p>
           Philip's app! ⭐ Bingo. Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -8,6 +10,7 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDsimojrGJ2chAOhvqmoKnHCrJ4gBLFMG8",
+    // authDomain: "localhost",
     authDomain: "pokertracker-23081.firebaseapp.com",
     projectId: "pokertracker-23081",
     storageBucket: "pokertracker-23081.appspot.com",
@@ -17,7 +20,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebase = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebase);
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+const auth = getAuth(firebaseApp);
+const authGoogle = new GoogleAuthProvider();
 
-export default firebase;
+export default firebaseApp;
+export {analytics, auth, authGoogle};
