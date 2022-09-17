3e5ea820532995a2830f74fc9dc7857e9322446b
Philip Brocoum <philip.brocoum@gmail.com>
2022-09-16 22:42:24 -0700

    PnlCard.js mock

--- src/PnlCard.js
+++ src/PnlCard.js
@@ -1,43 +1,17 @@
 import * as React from 'react';
-import {Box, Card, CardActions, CardContent, Button, Typography} from '@mui/material';
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
-
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        be{bull}nev{bull}o{bull}lent
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </React.Fragment>
-);
+import {Card, CardContent, Typography} from '@mui/material';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 
 export class PnlCard extends React.Component {
   render() {
     return (
-      <Card variant="outlined">{card}</Card>
+      <Card>
+        <CardContent>
+          <AccountBalanceIcon sx={{ color: 'text.disabled', position: 'absolute'}}/>
+          <Typography variant="h6" align="center">Bankroll</Typography>
+          <Typography variant="h4" align="center" sx={{ color: 'success.main' }}>+$1,000,000</Typography>
+        </CardContent>
+      </Card>
     );
   }
 }
