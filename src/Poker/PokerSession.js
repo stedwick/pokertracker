beb12a1637fd60f760521fb2e424d6927801393c
Philip Brocoum <philip.brocoum@gmail.com>
2022-09-19 22:19:43 -0700

    Sessions outline

--- src/Poker/PokerSession.js
+++ src/Poker/PokerSession.js
@@ -1,17 +1,29 @@
 import * as React from 'react';
 
-import {Card, CardContent, Accordion, AccordionSummary, AccordionDetails, Typography} from '@mui/material';
+import {Box, Card, CardContent, Accordion, AccordionSummary, AccordionDetails, Typography, Button} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import TableBarIcon from '@mui/icons-material/TableBar';
+import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
+import LocalAtmOutlinedIcon from '@mui/icons-material/LocalAtmOutlined';
+import AddIcon from '@mui/icons-material/Add';
 
 function PokerSession() {
   return (
     <Card variant="outlined">
-      <CardContent>
+      <CardContent sx={{position: 'relative'}}>
+        <TableBarIcon sx={{ color: 'text.disabled', position: 'absolute'}}/>
+        <Box sx={{position: "absolute", right: '0', marginRight: 2}}>
+          <Button variant="contained" size="small" startIcon={<AddIcon />}>
+            New
+          </Button>
+        </Box>
         <Typography variant="h6" align="center">Sessions</Typography>
         <br/>
         <Accordion>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>Accordion 1</Typography>
+            <EmojiEventsOutlinedIcon />
+            <Typography color="error" sx={{fontWeight: 'bold'}}>&nbsp;•&nbsp;</Typography>
+            <Typography>Mirage</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
@@ -22,7 +34,9 @@ function PokerSession() {
         </Accordion>
         <Accordion>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>Accordion 2</Typography>
+            <LocalAtmOutlinedIcon/>
+            <Typography color="error" sx={{visibility: "hidden", fontWeight: 'bold'}}>&nbsp;•&nbsp;</Typography>
+            <Typography>Wynn</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
