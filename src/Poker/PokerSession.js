3dd4f5b5a03e64236c635561371e4f60ecb71eaa
Philip Brocoum <philip.brocoum@gmail.com>
2022-09-19 23:56:16 -0700

    New session button re-factoring with onClick

--- src/Poker/PokerSession.js
+++ src/Poker/PokerSession.js
@@ -1,55 +1,33 @@
 import * as React from 'react';
 
-import {Box, Card, CardContent, Accordion, AccordionSummary, AccordionDetails, Typography, Button} from '@mui/material';
+import {Accordion, AccordionSummary, AccordionDetails, Typography} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import TableBarIcon from '@mui/icons-material/TableBar';
 import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
-import LocalAtmOutlinedIcon from '@mui/icons-material/LocalAtmOutlined';
-import AddIcon from '@mui/icons-material/Add';
 
-function PokerSession() {
-  return (
-    <Card variant="outlined">
-      <CardContent sx={{position: 'relative'}}>
-        <Typography variant="h6" sx={{ color: 'text.disabled', position: 'absolute'}}>
-          <TableBarIcon sx={{ verticalAlign: 'sub'}}/>
-        </Typography>
-        <Box sx={{position: "absolute", right: '0', marginRight: 2}}>
-          <Button variant="contained" size="small" startIcon={<AddIcon />}>
-            New
-          </Button>
-        </Box>
-        <Typography variant="h6" align="center">Sessions</Typography>
-        <br/>
-        <Accordion>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <EmojiEventsOutlinedIcon />
-            <Typography color="error" sx={{fontWeight: 'bold'}}>&nbsp;•&nbsp;</Typography>
-            <Typography>Mirage</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <LocalAtmOutlinedIcon/>
-            <Typography color="error" sx={{visibility: "hidden", fontWeight: 'bold'}}>&nbsp;•&nbsp;</Typography>
-            <Typography>Wynn</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      </CardContent>
-    </Card>
-  );
+export class PokerSession extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const location = this.props.details.location;
+
+    return (
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <EmojiEventsOutlinedIcon />
+          <Typography color="error" sx={{fontWeight: 'bold'}}>&nbsp;•&nbsp;</Typography>
+          <Typography>{location}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    );
+  }
 }
 
 export default PokerSession;
--- src/Poker/PokerSessions.js
+++ src/Poker/PokerSessions.js
@@ -1,10 +1,48 @@
 import * as React from 'react';
 import PokerSession from "./PokerSession.js";
+import {Box, Button, Card, CardContent, Typography} from "@mui/material";
+import TableBarIcon from "@mui/icons-material/TableBar";
+import AddIcon from "@mui/icons-material/Add";
 
-function PokerSessions() {
-  return (
-    <PokerSession/>
-  );
+export class PokerSessions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {pokerSessions: [
+      {location: "Mirage"},
+      {location: "Wynn"},
+      {location: "Aria"}
+    ]};
+  }
+
+  addPokerSession = () => {
+    this.setState(prevState => (
+      {pokerSessions: [{location: "Lorem"}, ...prevState.pokerSessions]})
+    );
+  }
+
+  render() {
+    const pokerSessions = this.state.pokerSessions;
+
+    return (
+      <Card variant="outlined">
+        <CardContent sx={{position: 'relative'}}>
+          <Typography variant="h6" sx={{ color: 'text.disabled', position: 'absolute'}}>
+            <TableBarIcon sx={{ verticalAlign: 'sub'}}/>
+          </Typography>
+          <Box sx={{position: "absolute", right: '0', marginRight: 2}}>
+            <Button variant="contained" size="small" startIcon={<AddIcon />} onClick={this.addPokerSession}>
+              New
+            </Button>
+          </Box>
+          <Typography variant="h6" align="center">Sessions</Typography>
+          <br/>
+          {pokerSessions.map((pokerSession, index)=>{
+            return <PokerSession key={index} details={pokerSession}/>
+          })}
+        </CardContent>
+      </Card>
+    );
+  }
 }
 
 export default PokerSessions;
